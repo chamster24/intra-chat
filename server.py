ebdb1d124743209be99993fe63df3bad3859962f
chamster24 <227488190+chamster24@users.noreply.github.com>
2025-11-09 16:56:52 -0700

    Refactor WebSocket connections and comments
    Updated WebSocket connection handling and improved comments.

--- server.py
+++ server.py
@@ -1,27 +1,22 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Serve your HTML
+# Serve your HTML/JS/CSS
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-# WebSocket connections
-connections = {}
+### ABOVE WAS TO HOST SERVER
+
+connections = []  # store connections temporarily
 
 @app.websocket("/ws/{room}")
 async def websocket_endpoint(websocket: WebSocket, room: str):
     await websocket.accept()
-    if room not in connections:
-        connections[room] = []
-    connections[room].append(websocket)
+    connections.append(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
-            # Broadcast to everyone in room
-            for conn in connections[room]:
-                if conn != websocket:
-                    await conn.send_text(data)
+            # just receive messages without doing anything
+            await websocket.receive_text()
     except Exception:
-        connections[room].remove(websocket)
+        connections.remove(websocket)
